e6efa997603132af15945b0958eb549d2a63c044
Miftahul93 <miftahululum693@gmail.com>
2018-12-03 21:01:48 +0700

    Belajar Android Pemula Menghitung Balok

---
+++ app/src/main/java/com/example/denmas_mieftahh/belajarandroidpemula/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.denmas_mieftahh.belajarandroidpemula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
